03973fa403d6ebc3adc1a185acbf4536c9f25639
Dhvani Rawal <143869314+dhvanirawal@users.noreply.github.com>
2026-05-13 13:33:32 +0530

    Task finished

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#D62062",
+        "primary-light": "#FF1376",
+        "primary-pink": "#F998BB",
+        dark: "#131313",
+        "dark-footer": "#1E1E1E",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
